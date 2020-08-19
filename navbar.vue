d67e93f8bc81588d53a1d8b3611089330f79ca0f
Lourvens <63268101+Lourvens@users.noreply.github.com>
2020-08-19 16:01:35 -0400

    Update navbar.vue

--- navbar.vue
+++ navbar.vue
@@ -4,31 +4,19 @@
         if(navbarExpanded) navbarExpanded = !navbarExpanded
     }"
 >
-    <v-row noGutters class="align-center">
+    <v-row no-gutters class="align-center">
     <v-col>
-        <v-toolbar-title style="font-family: 'Baloo Tamma 2'">
-        Travel Agency
+        <v-toolbar-title>
+         vuetify-expanded navbar
         </v-toolbar-title>
     </v-col>
     <v-spacer></v-spacer>
-    <v-col cols="8" class="hidden-sm-and-down">
+    <v-col class="hidden-sm-and-down">
         <v-toolbar-items class="d-inline-flex">
         <v-btn text v-for="link in links" :key="link.name">
             {{link.name}}
         </v-btn>
         </v-toolbar-items>
-        |
-        <v-tooltip v-for="icon in icons" :key="icon.name" bottom>
-        <template v-slot:activator="{ attrs, on}">
-            <v-btn icon rounded v-on="on" v-bind="attrs">
-                <v-icon small>
-                    // 
-                    fa fa-{{icon.name}} 
-                </v-icon>
-            </v-btn>
-        </template>
-        <span>{{icon.name}}</span>
-        </v-tooltip>
     </v-col>
     <v-btn icon class="hidden-md-and-up" @click="navbarExpanded = !navbarExpanded">
         <v-icon small>fa fa-{{navbarExpanded?'times':'bars'}}</v-icon>
@@ -38,11 +26,6 @@
         <v-btn block depressed x-large v-for="link in links" :key="link.name" class="my-4">
         {{link.name}}
         </v-btn>
-        <div class="mt4 d-flex justify-center ">
-        <v-btn icon v-for="icon in icons" :key="icon">
-            <v-icon>fa fa-{{icon.name}}</v-icon>
-        </v-btn>
-        </div>
     </v-col>
     </v-expand-transition>
     </v-row>
@@ -56,21 +39,9 @@ export default {
   data: () => ({
     links: [
       { name: 'home' },
-      { name: 'explore' },
-      { name: 'tours' },
+      { name: 'blog' },
       { name: 'about us' },
       { name: 'contact' },
-    ],
-    icons: [
-      { name: 'facebook' },
-      { name: 'instagram' },
-      { name: 'twitter' },
-      { name: 'linkedin' },
-    ],
-    cards : [
-      { name: 'nature', link: 'nature.jpg'},
-      { name: 'travel', link: 'travel.jpg'},
-      { name: 'beach', link: 'beach.jpg'},
     ],
     navbarExpanded: false
   })
